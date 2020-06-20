ea04b33a627cd7f72910b68d6f37dba9ac1acdd9
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-20 02:05:04 +0200

    style: run prettier

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,7 @@ import { TimingObject } from 'timing-object';
 import { TimingProvider } from 'timing-provider';
 
 const APPID_MCORP = '3705418343321362065';
-const app = MCorp.app(APPID_MCORP, { anon: true, range: [ 0, 4294967040 ] }); // eslint-disable-line no-undef
+const app = MCorp.app(APPID_MCORP, { anon: true, range: [0, 4294967040] }); // eslint-disable-line no-undef
 const $leftPlain = document.getElementById('left-plain');
 const $changeColorButton = document.getElementById('change-color');
 const $connectingMessageSpan = document.getElementById('connecting-message');
@@ -26,49 +26,43 @@ const renderColor = ($plain, timingObject) => {
 
     uint32Array[0] = position;
 
-    $plain.style.backgroundColor = `rgb(${ uint8Array[1] },${ uint8Array[2] },${ uint8Array[3] })`;
+    $plain.style.backgroundColor = `rgb(${uint8Array[1]},${uint8Array[2]},${uint8Array[3]})`;
 };
 
-Promise
-    .all([
-        app
-            .ready
-            .then(() => {
-                const timingObject = new TIMINGSRC.TimingObject({ provider: app.motions.shared }); // eslint-disable-line no-undef
+Promise.all([
+    app.ready.then(() => {
+        const timingObject = new TIMINGSRC.TimingObject({ provider: app.motions.shared }); // eslint-disable-line no-undef
 
-                return timingObject
-                    .ready
-                    .then(() => timingObject);
-            }),
-        new Promise((resolve) => {
-            const timingObject = new TimingObject(new TimingProvider('abcdefghijklmno01234'));
-            const onReadystateChange = () => {
-                timingObject.removeEventListener('readystatechange', onReadystateChange);
+        return timingObject.ready.then(() => timingObject);
+    }),
+    new Promise((resolve) => {
+        const timingObject = new TimingObject(new TimingProvider('abcdefghijklmno01234'));
+        const onReadystateChange = () => {
+            timingObject.removeEventListener('readystatechange', onReadystateChange);
 
-                if (timingObject.readyState === 'open') {
-                    resolve(timingObject);
-                }
-            };
-
-            timingObject.addEventListener('readystatechange', onReadystateChange);
-        })
-    ])
-    .then(([ timingSrcTimingObject, webRtcTimingObject ]) => {
-        $connectingMessageSpan.style.display = 'none';
-
-        $changeColorButton.style.display = 'block';
-        $changeColorButton.addEventListener('click', () => {
-            changeColor(timingSrcTimingObject, webRtcTimingObject);
-        });
-
-        const updateColor = () => {
-            renderColor($leftPlain, timingSrcTimingObject);
-            renderColor($rightPlain, webRtcTimingObject);
-
-            requestAnimationFrame(() => updateColor());
+            if (timingObject.readyState === 'open') {
+                resolve(timingObject);
+            }
         };
 
-        requestAnimationFrame(() => updateColor());
+        timingObject.addEventListener('readystatechange', onReadystateChange);
+    })
+]).then(([timingSrcTimingObject, webRtcTimingObject]) => {
+    $connectingMessageSpan.style.display = 'none';
+
+    $changeColorButton.style.display = 'block';
+    $changeColorButton.addEventListener('click', () => {
+        changeColor(timingSrcTimingObject, webRtcTimingObject);
     });
+
+    const updateColor = () => {
+        renderColor($leftPlain, timingSrcTimingObject);
+        renderColor($rightPlain, webRtcTimingObject);
+
+        requestAnimationFrame(() => updateColor());
+    };
+
+    requestAnimationFrame(() => updateColor());
+});
 
 app.init();
